7805aa08bfb99b66cf34e426e6125718e1bf1010
ap <47916875+atomicparade@users.noreply.github.com>
2022-01-09 12:47:52 -0500

    Add display for application messages

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,31 @@ import { createScheduler, createWorker } from 'tesseract.js';
 import IntRange from './intrange';
 import IntRangeSet from './intrangeset';
 
-console.log('Hello, world!');
+function setAppMessage(message: string, messageType: 'info' | 'error' = 'info'): void {
+  const pAppMessage = document.getElementById('appMessage');
+
+  if (!pAppMessage) {
+    throw new Error(`Unable to obtain <p id="appMessage"> to display ${messageType} message: ${message}`);
+  }
+
+  while (pAppMessage.firstChild !== null) {
+    pAppMessage.removeChild(pAppMessage.firstChild);
+  }
+
+  pAppMessage.classList.remove('info');
+  pAppMessage.classList.remove('error');
+  pAppMessage.classList.add(messageType);
+  pAppMessage.appendChild(document.createTextNode(message));
+}
+
+async function handlePaste(event: ClipboardEvent): Promise<void> {
+  console.log(event);
+}
+
+async function init(): Promise<void> {
+  document.addEventListener('paste', handlePaste);
+
+  setAppMessage('Please paste an image.');
+}
+
+window.addEventListener('load', init);
